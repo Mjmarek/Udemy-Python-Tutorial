dfb13dce97c794aa418d3af3905c90e8cf3b0e0c
Mjmarek <monicamarek24@gmail.com>
2017-12-27 20:03:42 -0500

    Python numbers

---
+++ numbers.py
@@ -0,0 +1,14 @@
+print(2+10 * 10+3)
+print((2+10)*(10+3))
+
+a = 5
+print(a+a)
+a = a*5
+print(a)
+
+#use snake_casing instead of PascalCasing or camelCasing
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
